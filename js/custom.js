4fd4ead2410894b7bc68933fdd52263ea0772c3f
hevelmo <hevelmo060683@gmail.com>
2017-10-28 23:43:25 -0500

    commit

--- js/custom.js
+++ js/custom.js
@@ -124,7 +124,14 @@ $(window).on('click',function() {
    VIDEO BACKGROUND
 ============================================================== */
 $('.video-play').vide("img/video/video", {
-    posterType: "jpg"
+    volume: 1,
+    playbackRate: 1,
+    muted: true,
+    loop: true,
+    autoplay: true,
+    position: '50% 50%', // Similar to the CSS `background-position` property.
+    posterType: 'none', // Poster image type. "detect" — auto-detection; "none" — no poster; "jpg", "png", "gif",... - extensions.
+    resizing: true // Auto-resizing, read: https://github.com/VodkaBears/Vide#resizing
 });
 console.log("video");
 
