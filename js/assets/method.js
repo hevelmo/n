de4c7c6acad521103a76768bdcbea1736bba052f
hevelmo <hevelmo060683@gmail.com>
2017-11-05 18:12:04 -0600

    commit

---
+++ js/assets/method.js
@@ -0,0 +1,27 @@
+/*
+    Autor: Heriberto Velasco Mora
+    Proyect: Computadoras Ghia
+
+    [var] vars
+
+
+    //Method
+
+    [method] browser
+
+**/
+/*
+ *  [var] vars
+ */
+    /* */
+        var inc;
+        inc = 'nav__inc_';
+    /* */
+/* end -> [var] vars */
+/*
+ *  [method] artyom
+ */
+    /* */
+        var
+    /* */
+/* end -> [method] artyom */
