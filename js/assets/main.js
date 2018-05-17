23439ef8032f3f171ee142f4d8db9c8ef88acfee
Heriberto Velasco <hevelmo060683@gmail.com>
2018-05-17 16:32:53 -0500

    commit

--- js/assets/main.js
+++ js/assets/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    var gallery_slider = $('#gallery').bxSlider({
+    $('#gallery-logos').bxSlider({
         pager: false,
         controls: false,
         minSlides: 5,
@@ -9,7 +9,43 @@ $(document).ready(function() {
         ticker: true,
         speed: 90000
     });
+    $('#gallery-logros').bxSlider({
+        pager: false,
+        controls: false,
+        minSlides: 1,
+        maxSlides: 8,
+        slideWidth: 235,
+        ticker: true,
+        speed: 20000
+    });
 
+    /* ===========================================================
+       BACK TO TOP BUTTON
+    ============================================================== */
+    var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.top');
+
+    //hide or show the "back to top" link
+    $(window).on('scroll', function() {
+        ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('is-visible') : $back_to_top.removeClass('is-visible fade-out');
+        if( $(this).scrollTop() > offset_opacity ) {
+            $back_to_top.addClass('fade-out');
+        }
+    });
+
+    //smooth scroll to top
+    $back_to_top.on('click', function(event){
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0
+            }, scroll_top_duration
+        );
+    });
     
     /*
     */
